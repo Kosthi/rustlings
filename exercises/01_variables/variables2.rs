84b37a6fa66e6de7004a7383d12768ebc66ca374
Koschei <nitianzero@gmail.com>
2024-07-21 14:36:32 +0800

    finish variables

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -1,6 +1,9 @@
+use std::arch::aarch64::int8x8_t;
+use std::os::unix::raw::ino_t;
+
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    let x;
+    let x = 10;
 
     if x == 10 {
         println!("x is ten!");
